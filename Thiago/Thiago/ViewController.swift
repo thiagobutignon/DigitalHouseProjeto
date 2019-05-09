9c06441a403c338b9b1538759252357a198af480
Thiago Butignon <thiagobutignon@gmail.com>
2019-05-09 20:47:14 -0300

    Criado o projeto no Xcode

---
+++ Thiago/Thiago/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thiago
+//
+//  Created by Thiago B Claramunt on 09/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Thiago/ThiagoTests/ThiagoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ThiagoTests.swift
+//  ThiagoTests
+//
+//  Created by Thiago B Claramunt on 09/05/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import XCTest
+@testable import Thiago
+
+class ThiagoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
